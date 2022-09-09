adedacbdce6e6a5b9c8390a1554f4fc7bf7e9834
David Pobi <pobidavid@gmail.com>
2022-09-09 12:23:43 +0000

    cleaning build warning

--- src/Pages/Collectibles/Collectibles.tsx
+++ src/Pages/Collectibles/Collectibles.tsx
@@ -1,95 +1,83 @@
-import React,{ useEffect, useState }  from 'react';
+import React, { useEffect, useState } from "react";
 import "./Collectibles.scss";
 import { useMoralis } from "react-moralis";
-import { getUserOwnedAssets_Alchemy } from '../../Services/AssetsService';
-import { useSelector,useDispatch } from 'react-redux';
-import { getAssets } from '../../Store/actions';
-
+import { getUserOwnedAssets_Alchemy } from "../../Services/AssetsService";
+import { useSelector, useDispatch } from "react-redux";
+import { getAssets } from "../../Store/actions";
 
 const Collectibles: React.FC = () => {
   /** Controller */
- 
+
   const { isAuthenticated, user } = useMoralis();
   const [userAddress, setUserAddress] = useState("");
   const [startToken, setStartToken] = useState("");
   const [hasNextPage, setHasNextPage] = useState(true);
-  const collectibles: Array<any>  =  useSelector((state:any) => state.collectibles);
+  const collectibles: Array<any> = useSelector((state: any) => state.collectibles);
   const dispatch = useDispatch();
 
-
- 
   useEffect(() => {
-      if(!isAuthenticated) {return;}
-        setUserAddress(user?.get("ethAddress"));
+    if (!isAuthenticated) {
+      return;
+    }
+    setUserAddress(user?.get("ethAddress"));
   }, []);
-
-
 
   useEffect(() => {
     console.log(userAddress);
-    if(userAddress) {
+    if (userAddress) {
       getUserAssets();
     }
-  },[userAddress]);
-
-
+  }, [userAddress]);
 
   const getUserAssets = async () => {
-    if(!hasNextPage) { 
-      console.log('done fetching');
+    if (!hasNextPage) {
+      console.log("done fetching");
       return;
     }
 
-    const result = await getUserOwnedAssets_Alchemy("eth",userAddress, startToken);
+    const result = await getUserOwnedAssets_Alchemy("eth", userAddress, startToken);
     console.log(result.assets);
-    if(result.assets.length > 0) {
+    if (result.assets.length > 0) {
       dispatch(getAssets(result.assets));
     }
-    if(!result.nextToken) {
+    if (!result.nextToken) {
       setHasNextPage(false);
       return;
     }
 
     setStartToken(result.nextToken);
-   }
- 
-  
-
+  };
 
   /** View */
   return (
- 
-    <> 
-        <div className='page-header'>
-          <h1 className='heading'>NFTs</h1>
-          <button  onClick={() => getUserAssets()}  className="btn btn-md btn-primary getCollectibles">Fetch NFTs</button> 
-        </div>
- 
-          <div className='assets-list'>
-            <ul className='list-inline'>
-           
-              {
-                collectibles.map((asset) => {
+    <>
+      <div className="page-header">
+        <h1 className="heading">NFTs</h1>
+        <button onClick={() => getUserAssets()} className="btn btn-md btn-primary getCollectibles">
+          Fetch NFTs
+        </button>
+      </div>
 
-                    return  (
-                    <li className="list-inline-item center" key={asset.id.tokenId}>
-                      <div className="asset-card">
-                        {
-                          asset.metadata.image_url ?  <img src={asset.metadata.image_url} className="preview"/> :   <img src={asset.media[0].gateway} className="preview"/>
-                        }
-                      <label className='name center'>{asset.metadata.name}</label>
-                      </div>
-
-                     </li>
-                      )
-                  })
-              }
-       
-            </ul>
-          </div>
+      <div className="assets-list">
+        <ul className="list-inline">
+          {collectibles.map((asset) => {
+            return (
+              <li className="list-inline-item center" key={asset.id.tokenId}>
+                <div className="asset-card">
+                  {asset.metadata.image_url ? (
+                    <img src={asset.metadata.image_url} className="preview" alt=".." />
+                  ) : (
+                    <img src={asset.media[0].gateway} className="preview" alt=".." />
+                  )}
+                  <label className="name center">{asset.metadata.name}</label>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
-
-  )
-}
+  );
+};
 
 export default Collectibles;
--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,38 +1,23 @@
-import React from 'react';
-import './Layout.scss';
-import Login from '../../Components/Auth/Login';
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import "./Layout.scss";
+import { Outlet } from "react-router-dom";
 
-let title: string = 'Atomic';
+let title: string = "Atomic";
 
 const Layout: React.FC = () => {
   return (
     <div className="App">
-        <div className="header">
-         <label className="title">{title}</label>
+      <div className="header">
+        <label className="title">{title}</label>
 
-          {/* <ul className="ul-list-inline nav">
-            <li className="list-inline-item nav-item">
-              <Link to="/" className="navLink">Home</Link>
-              </li>
+        {/* <Login/> */}
+      </div>
 
-              <li className="list-inline-item">
-              <Link to="/assets" className="navLink">NFTs</Link>
-              </li>
-
-              <li className="list-inline-item">
-              <Link to="/collection/0x2765b02b022b1e3cc84afef86d7a21c14b79cec4" className="navLink">Collections</Link>
-              </li>
-          </ul>
-      
-         <Login/> */}
-        </div>
-
-        <div className="main">
-         <Outlet/>
-        </div>
-  </div>
-  )
-}
+      <div className="main">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
